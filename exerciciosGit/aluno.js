4c21ff28b57372133fde421fc4c2e9d97b66cf3b
D1ego-H3nriqu3 <dh6451013@gmail.com>
2026-09-22 10:23:29 -0300

    Verificação da 'atividade' - ALUNO

--- exerciciosGit/aluno.js
+++ exerciciosGit/aluno.js
@@ -6,7 +6,16 @@ let aluno = {
 
     exibirInformacoes: function(){
         console.log(`\nNome: ${this.nome}\nIdade: ${this.idade}\nCurso: ${this.curso}`)
+    },
+
+    verificacaoAtivo: function(){
+        if(this.ativo === true){
+            console.log("Situação: Aluno ativo!")
+        }else{
+            console.log("Situação: Aluno inativo!")
+        }
     }
 }
 
 aluno.exibirInformacoes()
+aluno.verificacaoAtivo()
