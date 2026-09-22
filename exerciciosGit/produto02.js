6ff5dfbe0d7c9d64079191cccfa8dde6c1a501a5
D1ego-H3nriqu3 <dh6451013@gmail.com>
2026-09-22 11:20:59 -0300

    Adicionado aviso estoque - PRODUTO

--- exerciciosGit/produto02.js
+++ exerciciosGit/produto02.js
@@ -7,12 +7,20 @@ let produto = {
 function exibirDados(x){
     let valorTotal = calcVAlorT(x.estoque,x.preco)
     console.log(`\nNome: ${x.nome}\nPreço: R$ ${x.preco}\nEstoque: ${x.estoque} unidades\nValor total em estoque: ${valorTotal}`)
-    
+    avisoEstoque(x.estoque)
 }
 
 function calcVAlorT(estoque, preco){
         let valorTotal = estoque * preco
         return valorTotal
+}
+
+function avisoEstoque(estoque){
+    if(estoque <= 5){
+        console.log("Situação do estoque: baixo\n")
+    }else{
+        console.log("Situação do estoque: normal\n")
+    }
 }
 
 exibirDados(produto)
