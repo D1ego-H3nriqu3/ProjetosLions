8e2f55c5d5564d62535dd53ab49711d0b312d272
D1ego-H3nriqu3 <dh6451013@gmail.com>
2026-09-22 10:09:25 -0300

    Adicionado o preço total em estoque

--- exerciciosGit/produto.js
+++ exerciciosGit/produto.js
@@ -1,8 +1,18 @@
 const produto = {
 nome: "Fone de Ouvido",
 preco: 89.9,
-estoque: 15
+estoque: 15,
+
+    calcularValor: function(){
+        let valorTotal = this.preco * this.estoque
+        return valorTotal
+    }
 };
 console.log("Produto: " + produto.nome);
 console.log("Preço: R$ " + produto.preco);
 console.log("Estoque: " + produto.estoque + " unidades");
+
+let valorTotal = produto.calcularValor()
+
+console.log("Preço total em estoque: R$ " + valorTotal);
+
