a48597d6160b7ebd798fa1351d52195991bd4b6f
D1ego-H3nriqu3 <dh6451013@gmail.com>
2026-09-22 09:02:41 -0300

    Adicionado o cartão.js

---
+++ exerciciosGit/cartao.js
@@ -0,0 +1,9 @@
+let nome = "Diego"
+let idade = 15
+let cidade = "Ponta Grossa"
+let curso = "Desenvolvimento de Sistemas"
+
+console.log(`Nome ${nome}`)
+console.log(`Idade ${idade}`)
+console.log(`Cidade: ${cidade}`)
+console.log(`Curso: ${curso}`)
