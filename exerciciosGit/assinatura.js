a0f521d001987de91c0fe024b364300c3efa1c12
D1ego-H3nriqu3 <dh6451013@gmail.com>
2026-09-22 10:57:36 -0300

    Verificação tipo cliente - ASSINATURA

--- exerciciosGit/assinatura.js
+++ exerciciosGit/assinatura.js
@@ -5,7 +5,18 @@ let assinatura = {
 
     exibirDados: function(){
         console.log(`\nNome: ${this.nome}\nPlano: ${this.plano}\nMeses ativo: ${this.mesesAtivo}`)
+    },
+
+    tipoCliente: function(){
+        if(this.mesesAtivo < 6){
+            console.log("Classificação: cliente novo")
+        }else if(this.mesesAtivo >= 6 && this.mesesAtivo <= 23){
+            console.log("Classificação: cliente fiel")
+        }else{
+            console.log("Classificação: cliente veterano")
+        }
     }
 }
 
 assinatura.exibirDados()
+assinatura.tipoCliente()
