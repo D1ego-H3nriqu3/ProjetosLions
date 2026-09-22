5a72fbbe4719870020b14edbc572048dadb8e732
D1ego-H3nriqu3 <dh6451013@gmail.com>
2026-09-22 11:17:39 -0300

    Calculo valor total estoque - PRODUTO

--- exerciciosGit/produto02.js
+++ exerciciosGit/produto02.js
@@ -1,11 +1,19 @@
 let produto = {
     nome: "Teclado Mecânico",
-    preco: 249.99,
+    preco: 249.9,
     estoque: 8
 }
 
 function exibirDados(x){
-    console.log(`\nNome: ${x.nome}\nPreço: R$ ${x.preco}\nEstoque: ${x.estoque} unidades`)
+    let valorTotal = calcVAlorT(x.estoque,x.preco)
+    console.log(`\nNome: ${x.nome}\nPreço: R$ ${x.preco}\nEstoque: ${x.estoque} unidades\nValor total em estoque: ${valorTotal}`)
+    
+}
+
+function calcVAlorT(estoque, preco){
+        let valorTotal = estoque * preco
+        return valorTotal
 }
 
 exibirDados(produto)
+
